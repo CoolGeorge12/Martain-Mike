441489177ca72436fcbf29e7be4e51216d17a0b6
CoolGeorge <zack87608@gmail.com>
2025-09-29 09:57:03 -0600

    STUFF

---
+++ martain-mike/scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+@export var gravity = 400
+@export var jump_force = 200
+@export var speed = 125
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y += gravity * delta
+		if velocity.y == 500:
+			velocity.y = 500
+	else:
+			velocity.y = 0
+	
+	if Input.is_action_just_pressed ("jump") && is_on_floor() :
+		velocity.y = -jump_force
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
+	
+	velocity.x = direction * speed
+	move_and_slide()
+	
+	update_animations(direction)
+
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
