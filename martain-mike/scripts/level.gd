b02342cc982429cc98d62898ba0ab01060ea5e33
CoolGeorge <zack87608@gmail.com>
2025-11-23 19:18:08 -0700

    changes

--- martain-mike/scripts/level.gd
+++ martain-mike/scripts/level.gd
@@ -1,14 +1,51 @@
 extends Node2D
 
-@onready var start_position: Marker2D = $StartPosition
+@export var next_level: PackedScene = null
 
-@onready var player: Player = $Player
+@onready var start: StaticBody2D = $Start
+@onready var exit: Area2D = $Exit
+@onready var deathzone: Area2D = $Deathzone
+
+@onready var hud = $UILayer/HUD
+
+var player = null
+
+@export var level_time = 5
+
+var timer_node = null
+var time_left
+
+var win = false
 
 func _ready():
-	var array = [1, 2, 3]
-	array[0] = "first"
-	print(array[0])
-	print("Size of array: " + str(array.size()))
+	player = get_tree().get_first_node_in_group("player")
+	if player!= null:
+		player.global_position = start.get_spawn_pos()
+	var traps = get_tree().get_nodes_in_group("traps")
+	for trap in traps:
+		#trap.connect("touched_player", _on_trap_touched_player)
+		trap.touched_player.connect(_on_trap_touched_player)
+	
+	exit.body_entered.connect(_on_exit_body_entered)
+	deathzone.body_entered.connect(_on_deathzone_body_entered)
+	
+	time_left = level_time
+	hud.set_time_label(time_left)
+	
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
+
+func _on_level_timer_timeout():
+	if win == false:
+		time_left -= 1
+		hud.set_time_label(time_left)
+		if time_left < 0:
+			reset_player()
+			time_left = level_time
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
@@ -16,12 +53,21 @@ func _process(delta: float) -> void:
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
 
-
 func _on_deathzone_body_entered(body: Node2D) -> void:
 	reset_player()
+
 func _on_trap_touched_player() -> void:
 	reset_player()
 
 func reset_player():
 	player.velocity = Vector2.ZERO
-	player.global_position = start_position.global_position
+	player.global_position = start.get_spawn_pos()
+
+func _on_exit_body_entered(body):
+	if body is Player:
+		if next_level!=null:
+			exit.animate()
+			player.active = false
+			win = true
+			await get_tree().create_timer(1.5).timeout
+			get_tree().change_scene_to_packed(next_level)
